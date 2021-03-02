74702257201697db1c301eea70418a4ea56d56db
carl-larson <clarson08@gmail.com>
2021-03-01 19:31:16 -0600

    added files for routers and controllers, server connection works on backend.

---
+++ react/user-login/server/index.js
@@ -0,0 +1,34 @@
+//dependencies
+require('dotenv').config()
+const express = require("express");
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
+//routers
+const usersRouter = require('./routers/users');
+// const authRouter = require('./routers/auth');
+
+//more initializing
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json())
+app.use('/', express.static('client/build'), usersRouter)
+// app.use('/auth', authRouter)
+
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to our server!');
+// })
+
+app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}!`);
+});
