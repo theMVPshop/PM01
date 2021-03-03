394bdcefc862f80d42669abea8463928fb52670d
carl-larson <clarson08@gmail.com>
2021-03-02 19:59:42 -0600

    Set up React Router and basic pathing, with placeholder functions for the components

--- react/user-login/src/components/Login.js
+++ react/user-login/src/components/Login.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+function Login() {
 
+    return(
+        <div className="App">
+            <h2>Login</h2>
+        </div>
+    )
+}
 
 export default Login
--- react/user-login/src/components/Signup.js
+++ react/user-login/src/components/Signup.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+function Signup() {
 
+    return(
+        <div className="App">
+            <h2>Signup</h2>
+        </div>
+    )
+}
 
 export default Signup
