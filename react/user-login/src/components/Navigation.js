584ab5d9e7ab93e8808d30f488e870fd79f19bc0
carl-larson <clarson08@gmail.com>
2021-03-05 18:56:41 -0600

    moved Netlify Identity code from Projects component to Navigation component.

--- react/user-login/src/components/Navigation.js
+++ react/user-login/src/components/Navigation.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import '../App.css'
 
-class Navigation extends React.Component {
+function initNetlifyIdentity() {
+    const script = document.createElement("script");
+
+    script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+}
+
+function openNetlifyModal() {
+    const netlifyIdentity = window.netlifyIdentity;
+
+    if(netlifyIdentity) {
+        netlifyIdentity.open();
+    } else {
+        console.log("netlifyIdentity not defined")
+    }
+}
+
+class NetlifyIdentity extends Component {
+    componentDidMount() {
+        initNetlifyIdentity();
+    }
+    render() {
+        return(<div></div>)
+    }
+}
+
+
+class Navigation extends Component {
 
     render() {
         return (
             <div className="NavHeader">
+                <NetlifyIdentity />
                 <h1>NAVIGATION</h1>
+                <h3 onClick={()=>{ openNetlifyModal() }}>Login</h3>
             </div>
         )
     }
