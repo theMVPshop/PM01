6bfb16f65d1c25c30e18c2945764cd61263220db
michaelscoggins <rhythmghosts@gmail.com>
2021-03-09 19:56:13 -0600

    implemented navigation and routing

--- react/src/App.js
+++ react/src/App.js
@@ -1,41 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import Navigation from "./components/Navigation";
 import ReactRouter from "./ReactRouter";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navigation />
-//       <ReactRouter />
-//     </div>
-//   );
-// }
+function App() {
+  const [apiResponse, setApiResponse] = React.useState("");
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-  callAPI() {
+  function callAPI() {
     fetch("http://localhost:4001/users")
       .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
+      .then((res) => setApiResponse(res))
       .catch((err) => err);
   }
 
-  componentDidMount() {
-    this.callAPI();
-  }
+  React.useEffect(() => {
+    callAPI();
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <ReactRouter />
-        <p>Here is the API call {this.state.apiResponse}.</p>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Navigation />
+      <ReactRouter />
+      <p>Here is the API call {apiResponse}.</p>
+    </div>
+  );
 }
 
 export default App;
