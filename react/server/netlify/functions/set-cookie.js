c858b4d6ab620457a320095df6aa96bdc63cb3a1
michaelscoggins <rhythmghosts@gmail.com>
2021-03-11 19:36:06 -0600

    obligatory push

--- react/server/netlify/functions/set-cookie.js
+++ react/server/netlify/functions/set-cookie.js
@@ -1,23 +1,23 @@
 const cookie = require("cookie");
 const axios = require("axios");
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
   const parsedBody = JSON.parse(event.body);
   const { token } = parsedBody;
-  
-  const twoWeeks = 14 * 24 * 3600000
-  
+
+  const twoWeeks = 14 * 24 * 3600000;
+
   const netlifyCookie = cookie.serialize("nf_jwt", token, {
     secure: true,
     path: "/",
-    maxAge: twoWeeks
+    maxAge: twoWeeks,
   });
 
   callback(null, {
     statusCode: 200,
     headers: {
       "Set-Cookie": netlifyCookie,
-      "Cache-Control": "no-cache"
+      "Cache-Control": "no-cache",
     },
-    body: JSON.stringify({token})
+    body: JSON.stringify({ token }),
   });
 };
