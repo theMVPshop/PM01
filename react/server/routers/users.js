48cc5628a399b469a5dc718e8cb3ef62466760cb
carl-larson <clarson08@gmail.com>
2021-03-12 19:54:58 -0600

    created some routers and controllers for milestone CRUD

--- react/server/routers/users.js
+++ react/server/routers/users.js
@@ -6,4 +6,10 @@ router.get('/users', usersController.getAllUsers)
 
 router.get('/projects', usersController.getAllProjects)
 
+router.get('/milestones', usersController.getMilestoneByProject)
+
+router.post('/milestones', usersController.createMilestone)
+
+router.delete('/milestones', usersController.deleteMilestoneById)
+
 module.exports = router;
