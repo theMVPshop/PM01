7b91150e1911d666cd23e7d5087ddf530b8f1d95
carl-larson <clarson08@gmail.com>
2021-03-22 20:00:37 -0500

    built out more routers and controllers for milestone CRUD.

---
+++ initialize.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS permissions, milestones, projects, users;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(50),
+  pword VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE projects (
+id INT NOT NULL AUTO_INCREMENT,
+project_name VARCHAR(50),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE milestones (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(50),
+subtitle VARCHAR(50),
+project_id INT NOT NULL,
+due_date VARCHAR(12),
+ms_status INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (project_id) REFERENCES projects (id)
+);
+
+CREATE TABLE permissions (
+id INT NOT NULL AUTO_INCREMENT,
+user_id INT NOT NULL,
+project_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES users (id),
+FOREIGN KEY (project_id) REFERENCES projects (id)
+);
+
+INSERT INTO users
+	(user_name)
+VALUES 
+  ("James"), ("TEST");
+
+INSERT INTO projects
+	(project_name)
+VALUES
+	("First Project"), ("Another Project");
+    
+INSERT INTO milestones
+	(title, subtitle, project_id, due_date, ms_status)
+VALUES
+	("Begin", "Talk about project", 1, "01/01/2021", 1),
+    ("Wireframe", "Show wireframes to client", 1, "01/01/2021", 0),
+    ("Begin", "Talk about project", 2, "01/01/2021", 1),
+    ("Wireframe", "Show to client", 2, "01/01/2021", 0),
+    ("Finish", "Project is completed", 2, "01/01/2021", 1),
+    ("Finish", "Project is completed", 1, "01/01/2021", 0);
