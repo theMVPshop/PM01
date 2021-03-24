7c2e54df7b8e41a1d6fb01a14387098b234c2b9c
carl-larson <clarson08@gmail.com>
2021-03-23 19:37:26 -0500

    milestones page retrieves milestones by project ID and displays them when the component loads.

--- react/src/components/Milestones.js
+++ react/src/components/Milestones.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { Button } from "react-bootstrap";
 import {
   VerticalTimeline,
@@ -17,6 +18,20 @@ function Milestones() {
     status: "TODO",
     id: counter,
   });
+  const [data, setData] = useState({ stones: [] });
+  let projectID = 1;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        `http://localhost:4001/milestones/${projectID}`,
+      );
+      // console.log('this is the result', result)
+      setTodos(result.data);
+    };
+    fetchData();
+    console.log('this is the todos', todos);
+  }, []);
 
   const onChange = (event) => {
     setInput((prevState) => ({
