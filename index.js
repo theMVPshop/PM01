e21328bce2a5e18551a3d84b8c604c4208b415d0
carl-larson <clarson08@gmail.com>
2021-03-26 19:42:26 -0500

    Trying to see user object created by identity

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ netlifyIdentity.on('login', user => console.log('login', user));
 netlifyIdentity.on('logout', () => console.log('Logged out'));
 netlifyIdentity.on('error', err => console.error('Error', err));
 netlifyIdentity.on('open', () => console.log('Widget opened'));
-netlifyIdentity.on('close', () => console.log('Widget closed'));
+netlifyIdentity.on('close', () => console.log('Widget closed', user));
 
 // Unbind from events
 netlifyIdentity.off('login'); // to unbind all registered handlers
