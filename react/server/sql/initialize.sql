d28a4b5a611bcdcee52ffc781f701744ec80c5e9
carl-larson <clarson08@gmail.com>
2021-03-30 20:12:13 -0500

    started implementing routers and controllers for devlog component.

--- react/server/sql/initialize.sql
+++ react/server/sql/initialize.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS users, projects, milestones;
+DROP TABLE IF EXISTS permissions, devlog, milestones, projects, users;
 
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
@@ -15,23 +15,32 @@ PRIMARY KEY (id)
 
 CREATE TABLE milestones (
 id INT NOT NULL AUTO_INCREMENT,
-username VARCHAR(50),
-ms_name VARCHAR(50),
+title VARCHAR(50),
+subtitle VARCHAR(50),
 project_id INT NOT NULL,
-priority INT NOT NULL,
-ms_status INT NOT NULL,
+due_date VARCHAR(12),
+ms_status VARCHAR(16),
+description VARCHAR(50),
 PRIMARY KEY (id),
-FOREIGN KEY (project_id) REFERENCES projects (id),
-FOREIGN KEY (username) REFERENCES users (user_name)
+FOREIGN KEY (project_id) REFERENCES projects (id)
+);
+
+CREATE TABLE devlog (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(50),
+description VARCHAR(50),
+project_id INT NOT NULL,
+time_stamp INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
 CREATE TABLE permissions (
 id INT NOT NULL AUTO_INCREMENT,
-user_name VARCHAR(50),
-role VARCHAR(50),
+user_id INT NOT NULL,
 project_id INT NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (user_name) REFERENCES users (user_name),
+FOREIGN KEY (user_id) REFERENCES users (id),
 FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
@@ -46,6 +55,16 @@ VALUES
 	("First Project"), ("Another Project");
     
 INSERT INTO milestones
-	(ms_name, project_id, priority, ms_status)
+	(title, subtitle, project_id, due_date, ms_status, description)
 VALUES
-	("Begin", 1, 1, 1), ("Wireframe", 1, 2, 0), ("Begin", 2, 1, 1), ("Wireframe", 2, 2, 0), ("Finish", 2, 3, 1), ("Finish", 1, 3, 0);
+	("Begin", "Talk about project", 1, "01/01/2021", "TODO", "NA"),
+    ("Wireframe", "Show wireframes to client", 1, "01/01/2021", "TODO", "NA"),
+    ("Begin", "Talk about project", 2, "01/01/2021", "TODO", "NA"),
+    ("Wireframe", "Show to client", 2, "01/01/2021", "TODO", "NA"),
+    ("Finish", "Project is completed", 2, "01/01/2021", "TODO", "NA"),
+    ("Finish", "Project is completed", 1, "01/01/2021", "TODO", "NA");
+    
+INSERT INTO devlog
+	(title, description, project_id, time_stamp)
+VALUES
+	("Start the project", "We will start work now.", 1, 123456789);
