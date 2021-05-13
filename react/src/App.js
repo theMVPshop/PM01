89a86e83cc6b243947bdf77442fbf18846d05943
michaelscoggins <rhythmghosts@gmail.com>
2021-05-13 18:10:27 -0500

    deploy attempt

--- react/src/App.js
+++ react/src/App.js
@@ -66,7 +66,6 @@ function App() {
         openNetlifyModal={openNetlifyModal}
       />
       <ReactRouter localStorageCurrentUser={localStorageCurrentUser} />
-      {/* <p>localStorage currentUser: {`welcome ${localStorageCurrentUser}`}</p> */}
     </>
   );
 }
